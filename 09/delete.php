f210c838f0ec0d989de512a297c737cf201b7c39
Yuki Nukii <y.nukii@gmail.com>
2017-06-22 21:48:34 +0900

    no message

--- 09/delete.php
+++ 09/delete.php
@@ -8,7 +8,7 @@ $pdo = db_con();
 
 //3.UPDATE gs_an_table SET ....; で更新(bindValue)
 //　基本的にinsert.phpの処理の流れです。
-$stmt = $pdo->prepare("DELETE FROM gs_an_table WHERE id=:id");
+$stmt = $pdo->prepare("DELETE FROM gs_bm_table WHERE id=:id");
 $stmt->bindValue(':id',$id, PDO::PARAM_INT);
 $status = $stmt->execute();
 
